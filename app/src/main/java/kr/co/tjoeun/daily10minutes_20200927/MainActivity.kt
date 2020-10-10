ca0eabc1cdf1c47e4b0abc7c9fd5ee9137ae7b34
조경진 <kj_cho@idealidea.co.kr>
2020-10-10 15:18:04 +0900

    서버에서 내려주는 프로젝트 목록 파싱 / 멤버변수 ArrayList에 담아주기

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
@@ -34,6 +34,31 @@ class MainActivity : BaseActivity() {
         ServerUtil.getRequestProjectList(object : ServerUtil.JsonResponseHandler {
             override fun onResponse(json: JSONObject) {
 
+                val dataObj = json.getJSONObject("data")
+                val projectsArr = dataObj.getJSONArray("projects")
+
+//                projectsArr의 내용물을 0~끝번까지 뽑아내자.
+
+                for (i in    0 until projectsArr.length()) {
+
+//                    상황에 맞는 JSONObject를 추출
+                    val projectObj = projectsArr.getJSONObject(i)
+
+//                    추출된 JSONObject를 가지고 => Project 클래스로 변환.
+
+                    val tempProject = Project()
+                    tempProject.id = projectObj.getInt("id")
+                    tempProject.title = projectObj.getString("title")
+                    tempProject.imageURL = projectObj.getString("img_url")
+                    tempProject.desc = projectObj.getString("description")
+
+//                    완성된 Project 클래스를 => mProjectList 에 추가.
+
+                    mProjectList.add(tempProject)
+
+                }
+
+
             }
 
         })
