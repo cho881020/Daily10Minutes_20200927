a30f97bc5742767589718233472578ffcd8a4ec7
조경진 <kj_cho@idealidea.co.kr>
2020-10-10 14:57:20 +0900

    메인화면 진입시 프로젝트 목록 조회 요청 서버 전송

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
@@ -2,8 +2,13 @@ package kr.co.tjoeun.daily10minutes_20200927
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.daily10minutes_20200927.datas.Project
+import kr.co.tjoeun.daily10minutes_20200927.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mProjectList = ArrayList<Project>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,21 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        getProjectListFromServer()
+    }
+
+//    서버에서 프로젝트 목록 가져오는 코드를 별도 함수로 분리.
+
+    fun getProjectListFromServer() {
+
+//        서버에 프로젝트 목록 요청 => 응답을 분석 => 목록에 담아주는 코드
+
+        ServerUtil.getRequestProjectList(object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+            }
+
+        })
 
     }
 
