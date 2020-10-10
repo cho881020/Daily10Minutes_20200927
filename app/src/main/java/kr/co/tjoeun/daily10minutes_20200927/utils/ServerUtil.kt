967199fa87316e8f8abc8367d0b70f3b54b43034
조경진 <kj_cho@idealidea.co.kr>
2020-10-10 10:31:02 +0900

    서버의 응답을 로그로만 ServerUtil에서 확인

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/utils/ServerUtil.kt
@@ -1,8 +1,9 @@
 package kr.co.tjoeun.daily10minutes_20200927.utils
 
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class ServerUtil {
 
@@ -36,7 +37,28 @@ class ServerUtil {
 
 //            완성된 Request를 가지고 실제 서버 연결 (클라이언트의 일) 코드
 
-            client.newCall(request)
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+//                    서버에 연결 자체를 실패한 경우. (물리적 실패)
+//                    인터넷 연결 실패 등등.
+//                    아이디/비번 틀림등 로그인 실패는 여기가 아님.
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+//                    검사 결과가 성공이던, 실패던 관계 없이 서버가 뭔가 답변을 해주면 무조건 실행.
+
+//                    서버가 내려준 응답중 본문(body)만 String 형태로 저장.
+                    val bodyString = response.body!!.string()
+
+//                    받아낸 String을 => 분석하기 용이한 JSONObject로 변환.
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("서버응답본문", jsonObj.toString())
+
+                }
+
+            })
 
         }
 
