b90ceab4a9bd1ed9981b26f9fb06b1c6927c682a
조경진 <kj_cho@idealidea.co.kr>
2020-10-10 14:16:18 +0900

    서버 데이터 저장용 프로젝트 데이터클래스 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/datas/Project.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.daily10minutes_20200927.datas
+
+class Project {
+
+//    서버에서 내려주는 참여가능 프로젝트의 데이터(하위정보) 를 담기 위한 변수들.
+
+    var id = 0 // id는 Int라고 명시.
+    var title = "" // title은 String이라고 명시.
+    var imageURL = ""
+    var desc = ""
+
+}
