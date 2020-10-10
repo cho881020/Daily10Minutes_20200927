f3e86c385d17ec714fc431017c39d4f6b254077f
조경진 <kj_cho@idealidea.co.kr>
2020-10-10 15:37:28 +0900

    리스트뷰 / 어댑터 연결 + 서버에서 다녀온 경우 리스트뷰 새로고침

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
@@ -2,6 +2,8 @@ package kr.co.tjoeun.daily10minutes_20200927
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.daily10minutes_20200927.adapters.ProjectAdapter
 import kr.co.tjoeun.daily10minutes_20200927.datas.Project
 import kr.co.tjoeun.daily10minutes_20200927.utils.ServerUtil
 import org.json.JSONObject
@@ -9,6 +11,8 @@ import org.json.JSONObject
 class MainActivity : BaseActivity() {
 
     val mProjectList = ArrayList<Project>()
+
+    lateinit var mAdapter : ProjectAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,10 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
         getProjectListFromServer()
+
+        mAdapter = ProjectAdapter(mContext, R.layout.project_list_item, mProjectList)
+        projectListView.adapter = mAdapter
+
     }
 
 //    서버에서 프로젝트 목록 가져오는 코드를 별도 함수로 분리.
@@ -57,6 +65,15 @@ class MainActivity : BaseActivity() {
                     mProjectList.add(tempProject)
 
                 }
+
+//                서버에 다녀오는 작업이, 어댑터 연결보다 늦게 완료될 수 있다. (거리상 문제)
+//                서버에 다녀오면 어댑터 연결 이후 => 데이터 추가 상황 발생 가능.
+//                내용물로 쓰이는 목록에 변경 발생. => 리스트뷰 새로고침 코드 추가
+
+                runOnUiThread {
+                    mAdapter.notifyDataSetChanged()
+                }
+
 
 
             }
