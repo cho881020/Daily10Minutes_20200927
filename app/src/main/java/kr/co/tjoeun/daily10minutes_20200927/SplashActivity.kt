8f6a4f9b5922b32bed5b1d7e7e2c775a9ec24dda
조경진 <kj_cho@idealidea.co.kr>
2020-10-11 11:09:41 +0900

    스플래쉬 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/SplashActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.daily10minutes_20200927
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
