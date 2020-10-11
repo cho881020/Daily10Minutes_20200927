126dc96987e4a41ad2d6254109cd7455a3985654
조경진 <kj_cho@idealidea.co.kr>
2020-10-11 15:11:08 +0900

    사용자 파싱시, 프사 목록을 같이 파싱

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/datas/User.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/datas/User.kt
@@ -9,6 +9,10 @@ class User : Serializable {
     var email = ""
     var nickName = ""
 
+//    여러장의 프로필사진? => 한명의 User : 프사 ArrayList로 갖는다.
+//    프사 데이터중, URL에 해당하는 String만 모아서 들고있자.
+    val profileImageList = ArrayList<String>()
+
     companion object {
 
         fun getUserFromJSON(json: JSONObject) : User {
@@ -18,6 +22,24 @@ class User : Serializable {
             u.id = json.getInt("id")
             u.email = json.getString("email")
             u.nickName = json.getString("nick_name")
+
+//            프사들을 파싱해서 목록에 채워주자.
+
+            val profileImageArr = json.getJSONArray("profile_images")
+
+//            JSON Array 내부를 반복으로 돌면서 => 프사를 한장씩 목록에 채우자.
+
+            for (i in    0 until profileImageArr.length() ) {
+
+                val profileImageObj = profileImageArr.getJSONObject(i)
+
+//                평소대로면 클래스로 변환, 지금은 간략하게 URL만 String을 추출하자.
+                val imageUrl = profileImageObj.getString("img_url")
+
+//                지금 파싱중인 사용자 u의 프사로 등록
+                u.profileImageList.add(imageUrl)
+
+            }
 
             return u
 
