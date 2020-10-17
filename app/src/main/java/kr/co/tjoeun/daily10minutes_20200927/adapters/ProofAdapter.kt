6428f9ce5a0198341d42a613704e7e9fb850df2a
조경진 <kj_cho@idealidea.co.kr>
2020-10-17 12:37:25 +0900

    어댑터에서 작성일시 양식 재가공 출력

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ProofAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ProofAdapter.kt
@@ -13,6 +13,7 @@ import kr.co.tjoeun.daily10minutes_20200927.R
 import kr.co.tjoeun.daily10minutes_20200927.datas.Project
 import kr.co.tjoeun.daily10minutes_20200927.datas.Proof
 import kr.co.tjoeun.daily10minutes_20200927.datas.User
+import java.text.SimpleDateFormat
 
 class ProofAdapter(
     val mContext: Context,
@@ -46,6 +47,9 @@ class ProofAdapter(
         Glide.with(mContext).load(proofData.writer.profileImageList[0]).into(writerProfileImg)
         writerNickNameTxt.text = proofData.writer.nickName
 
+//        인증글 시간정보 => 2020년 5월 8일 오후 3시 1분 양식으로 출력
+        val sdf = SimpleDateFormat("yyyy년 M월 d일 a h시 m분")
+        writtenDateTimeTxt.text = sdf.format(proofData.proofTime.time)
 
         return row
     }
