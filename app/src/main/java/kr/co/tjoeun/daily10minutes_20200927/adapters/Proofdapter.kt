ba4292eb3db78bccf81fc7439700e66ef36fad73
조경진 <kj_cho@idealidea.co.kr>
2020-10-17 12:07:34 +0900

    인증글 어댑터 뼈대작업

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/Proofdapter.kt
@@ -0,0 +1,36 @@
+package kr.co.tjoeun.daily10minutes_20200927.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import kr.co.tjoeun.daily10minutes_20200927.R
+import kr.co.tjoeun.daily10minutes_20200927.datas.Project
+import kr.co.tjoeun.daily10minutes_20200927.datas.Proof
+import kr.co.tjoeun.daily10minutes_20200927.datas.User
+
+class Proofdapter(
+    val mContext: Context,
+    resId: Int,
+    val mList: List<Proof>) : ArrayAdapter<Proof>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.proof_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+}
