9fd3fcc5549544585f0129a00f8f4bd5618d76fd
조경진 <kj_cho@idealidea.co.kr>
2020-10-17 10:12:41 +0900

    인증글 목록 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/ViewProofListActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.daily10minutes_20200927
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProofListActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_proof_list)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+
+    }
+
+}
