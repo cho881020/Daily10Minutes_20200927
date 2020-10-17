f96c01b2db93788d7e925a15cb11da2d6aba19b4
조경진 <kj_cho@idealidea.co.kr>
2020-10-17 15:47:58 +0900

    인증글 작성 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/EditProofActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.daily10minutes_20200927
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProofActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_proof)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
