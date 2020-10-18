8fa2682d37eb05d9384e912ddfb61571444a91a1
조경진 <kj_cho@idealidea.co.kr>
2020-10-18 10:36:21 +0900

    인증글 댓글 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/ViewProofReplyListActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.daily10minutes_20200927
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProofReplyListActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_proof_reply_list)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
