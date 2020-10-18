284cd762600d2d779207270f41376d84590a7276
조경진 <kj_cho@idealidea.co.kr>
2020-10-18 13:59:05 +0900

    액션바를 가지고 있는 모든 화면에 커스텀액션바 일괄 적용

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/BaseActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/BaseActivity.kt
@@ -1,5 +1,7 @@
 package kr.co.tjoeun.daily10minutes_20200927
 
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -11,11 +13,30 @@ abstract class BaseActivity : AppCompatActivity() {
     abstract fun setupEvents()
     abstract fun setValues()
 
+//    BaseActivity의 onCreate를 오버라이딩.
+//    상속받는 다른 모든 액티비티의 super.onCreate에서 이 코드를 실행함.
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        여기서 실행하는 코드는 => 모든 액티비티가 공통적으로 실행함.
+//        여기서 커스텀 액션바 세팅 => 모든 액티비티가 커스텀 액션바 세팅.
+
+//        액션바가 있을때만, 세팅하도록 상황 확인.
+        supportActionBar?.let {
+
+//            supportActionBar가 null 이 아닐때만 실행해줄 코드
+            setCustomActionBar()
+
+        }
+
+    }
+
 //    액션바를 커스터마이징 해주는 기능. (모든 액티비티가 공유 - BaseActivity에 작성)
 
     fun setCustomActionBar() {
 
-//        모든 액티비티에는 액션바가 있다고 전제하자.
+//        모든 액티비티에는 액션바가 있다고 전제하자. (Null에 대한 리스크 감수)
         val myActionBar = supportActionBar!!
 
 //        액션바가 커스텀 화면을 보여줄 수 있게 모드 설정
--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
-        setCustomActionBar()
     }
 
     override fun setupEvents() {
