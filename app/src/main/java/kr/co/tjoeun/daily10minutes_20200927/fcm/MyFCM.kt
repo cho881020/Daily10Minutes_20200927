d9ad314d2f6b49b9f764d40af6e8f09f8dcbdbf2
조경진 <kj_cho@idealidea.co.kr>
2020-10-18 15:21:13 +0900

    알림 받으면, 토스트로 제목 출력 예시  - applicationContext 활용

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/fcm/MyFCM.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/fcm/MyFCM.kt
@@ -1,6 +1,9 @@
 package kr.co.tjoeun.daily10minutes_20200927.fcm
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -12,8 +15,19 @@ class MyFCM : FirebaseMessagingService() {
 //        푸시알림이 들어왔을때 실행되는 함수.
 //        앱이 켜진상태에서 => 알림이 오면 실행됨.
 
-//        알림에 전송한 제목 로그 테스트
-        Log.d("푸시알림", p0.notification!!.title!!)
+//        알림에 전송한 제목 토스트로 출력
+//        Log.d("푸시알림", p0.notification!!.title!!)
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.post {
+
+//            UI 쓰레드에서 토스트 띄우기
+            Toast.makeText(applicationContext, p0.notification!!.title!!, Toast.LENGTH_SHORT).show()
+
+        }
+
+
 
     }
 
